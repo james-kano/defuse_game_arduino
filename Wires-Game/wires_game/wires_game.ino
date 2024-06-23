fa2b8cec36fd9c35023f42cdcafb8216a84c6d25
james-kano <161379017+james-kano@users.noreply.github.com>
2024-06-23 12:51:04 +0100

    Wires game (#4)
    * Wires game POC and code generator notebook
    
    * logic wrappers and single game code generator
    
    * signal and game indicatorstemplated
    
    * Working sketch with keyboard matrix-style rails (#3)
    
    * conflicts resolved
    
    ---------
    
    Co-authored-by: jameskanoNW <james.kano@natwest.com>

---
+++ Wires-Game/wires_game/wires_game.ino
@@ -0,0 +1,200 @@
+/*
+  Auto-generated wiring program template for wires game.
+
+  Reads against 3 states:
+   - High rail connected
+   - Low rail connected
+   - Disconnected
+
+  High rail should be connected to digital pin 5.
+  Low rail should be connected to digital pin 6.
+  All input wires should have 100K pull-down to GND, then conncted to inputs specified below. 
+*/
+
+
+const int win_sig_out = 5;
+const int lose_sig_out = 6;
+const int lose_sig_in = 7;
+
+const int high_rail = 10;
+const int low_rail = 11;
+
+const int red = A0;
+const int yellow = A1;
+const int orange = A2;
+const int white = A3;
+const int blue = A4;
+const int green = A5;
+
+const int wires[6] = {red, yellow, orange, white, blue, green};
+
+int curr_pin_states[6] = {0, 0, 0, 0, 0, 0};
+int allowed_mistakes[6] = {1, 1, 1, 1, 1, 1};
+
+//Start light pin declarations
+const int top = 2;
+const int middle = 3;
+const int bottom = 4;
+
+bool wire_change = false;
+int progress = 0;
+int mistake_count = 0;
+int lose_count = 3;
+
+
+int get_curr_pin_state(int pin) {
+  // Gets the current mapped state of a given pin
+  // Maps the input value against uneven thresholds for 3-level mapping
+  // open = 0, low = 1, high = 2
+  int val_in = 0;
+  digitalWrite(high_rail, HIGH);
+  if (digitalRead(pin) == HIGH) {
+    val_in = 2;
+  }
+  digitalWrite(high_rail, LOW);
+  digitalWrite(low_rail, HIGH);
+  if (digitalRead(pin) == HIGH) {
+    val_in = 1;
+  }
+  digitalWrite(low_rail, LOW);
+  return val_in;
+}
+
+
+void safe() {
+  // Actions upon game won
+  digitalWrite(win_sig_out, HIGH);
+}
+
+
+void lose () {
+  // Actions upon game lose
+  digitalWrite(lose_sig_out, HIGH);
+}
+
+
+bool check_mistakes() {
+  // Checks for disallowed mismatched wire states (mistakes)
+  bool mistake = false;
+  
+  ToDo: <<<<<<<<<<
+  
+  return mistake;
+}
+
+void check_progress() {
+  // Checks the progress and increments if successful
+  bool correct = false;
+  switch (progress) {
+    case 0:
+      if (curr_pin_states[2] == 2 && curr_pin_states[3] == 1) {
+        correct = true;
+      }
+      break;
+    case 1:
+      if (curr_pin_states[2] == 2 && curr_pin_states[3] == 1 && curr_pin_states[5] == 0) {
+        correct = true;
+      }
+      break;
+    case 2:
+      if (curr_pin_states[2] == 0 && curr_pin_states[3] == 1 && curr_pin_states[5] == 0 && curr_pin_states[1] == 2) {
+        correct = true;
+      }
+      break;
+    case 3:
+      if (curr_pin_states[2] == 0 && curr_pin_states[3] == 1 && curr_pin_states[5] == 0 && curr_pin_states[1] == 1 && curr_pin_states[4] == 0) {
+        correct = true;
+      }
+      break;
+    case 4:
+      if (curr_pin_states[2] == 0 && curr_pin_states[3] == 2 && curr_pin_states[5] == 0 && curr_pin_states[1] == 0 && curr_pin_states[4] == 0) {
+        correct = true;
+      }
+      break;
+  }
+  if (correct == true) {
+      // set curr_pin_states
+      for (int i=0; i<6; i++) {
+        int curr_pin_state = get_curr_pin_state(wires[i]);
+        int prev_pin_state = curr_pin_states[i];
+      }
+      progress ++;
+  }
+  else {
+    // check mistakes
+    bool mistake = check_mistakes();
+    if (mistake == true) {
+      mistake_count++;
+    }
+  }
+  Serial.print("Progress: ");
+  Serial.println(progress);
+}
+
+
+void setup() {
+  Serial.begin(9600);
+  // set the current wire conditions
+  for (int i=0; i<6; i++) {
+    int curr_pin_state = get_curr_pin_state(wires[i]);
+    curr_pin_states[i] = curr_pin_state;
+  }
+  // setup the win / lose signal wires
+  pinMode(win_sig_out, OUTPUT);
+  digitalWrite(win_sig_out, LOW);
+  pinMode(lose_sig_out, OUTPUT);
+  digitalWrite(lose_sig_out, LOW);
+  pinMode(lose_sig_in, INPUT);
+  // setup the rail pins;
+  pinMode(high_rail, OUTPUT);
+  digitalWrite(high_rail, LOW);
+  pinMode(low_rail, OUTPUT);
+  digitalWrite(low_rail, LOW);
+  // set the sequence indicator LEDs
+  pinMode(top, OUTPUT);
+  digitalWrite(top, HIGH);
+  pinMode(middle, OUTPUT);
+  digitalWrite(middle, HIGH);
+  pinMode(bottom, OUTPUT);
+  digitalWrite(bottom, HIGH);
+  // set the wire pins to be digital
+  pinMode(red, INPUT);
+  pinMode(yellow, INPUT);
+  pinMode(orange, INPUT);
+  pinMode(white, INPUT);
+  pinMode(blue, INPUT);
+  pinMode(green, INPUT);
+
+}
+
+
+void loop() {
+  // Game loop
+  wire_change = false;
+  // check for wire change
+  for (int i=0; i<6; i++) {
+    int curr_pin_state = get_curr_pin_state(wires[i]);
+    int prev_pin_state = curr_pin_states[i];
+    if (curr_pin_state != prev_pin_state) {
+      wire_change = true;
+      curr_pin_states[i] = curr_pin_state;
+      Serial.print("A");
+      Serial.print(i);
+      Serial.print(": ");
+      Serial.println(curr_pin_state);
+      delay(100);
+    }
+  }
+  if (wire_change == true && mistake_count < lose_count) {
+    check_progress();
+  }
+
+  if (progress == 5) {
+    safe();
+  }
+  else {
+    if (mistake_count == lose_count) {
+      lose();
+    }
+  }
+}
