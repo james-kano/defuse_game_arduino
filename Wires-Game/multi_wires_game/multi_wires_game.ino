c8de59ebc85eefe079cf0604d8ce8903ab651117
james-kano <james-kano@users.noreply.github.com>
2025-10-25 18:16:24 +0100

    Licensing added

--- Wires-Game/multi_wires_game/multi_wires_game.ino
+++ Wires-Game/multi_wires_game/multi_wires_game.ino
@@ -1,5 +1,8 @@
-/*
+/**************************************************************************
   Auto-generated wiring program template for wires game.
+
+  Copyright (C) 2025  James Kano
+
 
   Reads against 3 states:
    - High rail connected
@@ -9,7 +12,22 @@
   High rail should be connected to digital pin 5.
   Low rail should be connected to digital pin 6.
   All input wires should have 100K pull-down to GND, then conncted to inputs specified below. 
-*/
+
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+/**************************************************************************/
 
 
 const int win_sig_out = 10;
